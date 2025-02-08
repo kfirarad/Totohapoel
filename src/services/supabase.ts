df228491e8173c61e21c6537810f63242af02380
Kfir Arad <the.kfir@gmail.com>
2025-02-08 21:17:47 +0200

    gh-pages

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Get the URL and anon key from the VITE environment variables
+const supabaseUrl = "https://tabpznqluszwzreclwrb.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhYnB6bnFsdXN6d3pyZWNsd3JiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNDE5NDAsImV4cCI6MjA1MzgxNzk0MH0.eDOmOC36cJB46uowJInIFYv9kUmXiR6bLMLhuZO9VQ8"
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-}); 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
