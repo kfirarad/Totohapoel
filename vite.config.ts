caa366f45a8b37b8f9bf942847db1aed49a8ab95
Kfir Arad <the.kfir@gmail.com>
2025-02-08 20:47:39 +0200

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TotoHapoel/',
+  base: '/Totohapoel/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
